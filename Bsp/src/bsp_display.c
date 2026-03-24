5f5675defff3348a404f8187cf58e21c22af3956
haliwt <jonewang@126.com>
2026-03-24 15:28:58 +0800

    be  solved bug .DATA.2026.03.24

--- Bsp/src/bsp_display.c
+++ Bsp/src/bsp_display.c
@@ -116,7 +116,6 @@ void Display_TimeColon_Blink_Fun(void)
 
   if(gpro_t.set_timer_timing_doing_value==1 || gpro_t.set_timer_timing_doing_value==2) return ;
 
-  if(gpro_t.set_timer_timing_doing_value==3)
 
   if(run_t.gTimer_time_colon >0){ //200 //10*20ms=300ms
 
