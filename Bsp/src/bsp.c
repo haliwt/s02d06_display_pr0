510781ba39c67ffa11f20aee69e3c56df427170c
haliwt <jonewang@126.com>
2026-06-15 09:28:40 +0800

    update

--- Bsp/src/bsp.c
+++ Bsp/src/bsp.c
@@ -209,7 +209,7 @@ void power_off_run_handler(void)
 *******************************************************/
 void twoHours_works_timing(void)
 {
-   static uint8_t counter_send;
+  
    #if 1
    if(gpro_t.gTimer_work_counter_minutes>5 &&  gpro_t.two_work_hours_flag ==0){
 
@@ -224,7 +224,7 @@ void twoHours_works_timing(void)
       gpro_t.fan_run_one_minute =1; //one minute is flag .
 	  gpro_t.gTimer_counter_one_minute =0;
       SendData_Set_Command(0x19,0x01) ;//works two hours ,then have a rest 10 minutes.
-	  tx_thread_sleep(1);
+	  tx_thread_sleep(2);
      
    }
    #if 1
@@ -240,31 +240,31 @@ void twoHours_works_timing(void)
 		gpro_t.two_work_hours_flag = 0;
         gpro_t.fan_run_one_minute =3; //one minute is flag .
         SendData_Set_Command(0x19,0x0);
-	    tx_thread_sleep(3);
-	    //SendData_Set_Command(0x18,0x0);//fan run .
-	    //tx_thread_sleep(1);
+	    tx_thread_sleep(2);
+	   
    }
 
-  if(gpro_t.two_work_hours_flag == 1)counter_send++;
 
    
   if(gpro_t.fan_run_one_minute==1 && gpro_t.gTimer_counter_one_minute >59){
 		  gpro_t.fan_run_one_minute=2;
+		  gpro_t.gTimer_mode_long_key_counter=0;
 		  SendData_Set_Command(0x18,0x01);//fan stop run .
-		  tx_thread_sleep(3);
+		  tx_thread_sleep(2);
    }
    else if(gpro_t.fan_run_one_minute==3){
 	
 		   gpro_t.fan_run_one_minute++;
+		   
 		   SendData_Set_Command(0x18,0x0);//fan run .
-		   tx_thread_sleep(3);
+		   tx_thread_sleep(2);
 	
    }
-   else if(gpro_t.two_work_hours_flag ==1 && counter_send >5 &&	gpro_t.fan_run_one_minute ==2){
-		 counter_send=0;
-	
+   else if(gpro_t.two_work_hours_flag ==1 && gpro_t.gTimer_mode_long_key_counter >12 &&	gpro_t.fan_run_one_minute ==2){
+		
+	     gpro_t.gTimer_mode_long_key_counter=0;
 		 SendData_Set_Command(0x19,0x01);
-		 tx_thread_sleep(3);
+		 tx_thread_sleep(2);
 
    	}
 		 
